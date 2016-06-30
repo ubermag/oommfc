46089c6ff92b7fb054a944783c9b213d7dcd04b5
mb1a15 <mb4e10@soton.ac.uk>
2016-06-30 14:56:42 +0100

    Increase the version after bug fixes.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.rst') as f:
 
 setup(
     name='oommfc',
-    version='0.1',
+    version='0.2',
     description='A Python-based OOMMF calculator.',
     long_description=readme,
     author='Computational Modelling Group',
     author_email='fangohr@soton.ac.uk',
     url = 'https://github.com/joommf/oommfc',
-    download_url = 'https://github.com/joommf/oommfc/tarball/0.1',
+    download_url = 'https://github.com/joommf/oommfc/tarball/0.2',
     packages=['oommfc', 'oommfc.energies', 'oommfc.tests'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
