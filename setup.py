fbca95962a03c19e7a6d9c39773ce8768b96e664
mb1a15 <mb4e10@soton.ac.uk>
2016-07-04 12:11:20 +0100

    Modify setup.py to include dependencies during installation.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 with open('README.rst') as f:
     readme = f.read()
@@ -13,6 +13,11 @@ setup(
     url = 'https://github.com/joommf/oommfc',
     download_url = 'https://github.com/joommf/oommfc/tarball/0.2',
     packages=['oommfc', 'oommfc.energies', 'oommfc.tests'],
+    install_requires=[
+        'finitedifferencefield',
+        'oommffield',
+        'oommfodt'
+    ],
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
