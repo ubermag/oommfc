559dc9183cd7745e9b2438954d9b7d8ca7a8f065
mb1a15 <mb4e10@soton.ac.uk>
2016-09-22 16:57:28 +0100

    Restructure uniaxial anisotropy tests.

---
+++ oommfc/hamiltonian/uniaxialanisotropy.py
@@ -0,0 +1,12 @@
+from micromagneticmodel.hamiltonian import UniaxialAnisotropy
+
+
+class UniaxialAnisotropy(UniaxialAnisotropy):
+    def script(self):
+        mif = "# UniaxialAnisotropy\n"
+        mif += "Specify Oxs_UniaxialAnisotropy {\n"
+        mif += "  K1 {}\n".format(self.K)
+        mif += "  axis {{{} {} {}}}\n".format(self.u[0], self.u[1], self.u[2])
+        mif += "}\n\n"
+
+        return mif
---
+++ oommfc/tests/test_uniaxialanisotropy.py
@@ -0,0 +1,25 @@
+from micromagneticmodel.tests.test_uniaxialanisotropy import TestUniaxialAnisotropy
+from oommfc.hamiltonian import UniaxialAnisotropy
+
+
+class TestUniaxialAnisotropy(TestUniaxialAnisotropy):
+    def test_script(self):
+        for arg in self.valid_args:
+            K = arg[0]
+            u = arg[1]
+            name = "anisotropy_test"
+            
+            anisotropy = UniaxialAnisotropy(K, u, name=name)
+
+            script = anisotropy.script()
+            assert script.count("\n") == 6
+            assert script[0] == "#"
+            assert script[-1] == "\n"
+            
+            lines = script.split("\n")
+            assert len(lines) == 7
+            assert lines[0] == "# UniaxialAnisotropy"
+            assert lines[1] == "Specify Oxs_UniaxialAnisotropy {"
+            assert lines[2] == "  K1 {}".format(K)
+            assert lines[3] == "  axis {{{} {} {}}}".format(u[0], u[1], u[2])
+            assert lines[4] == "}"
