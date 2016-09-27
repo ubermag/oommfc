6225d1f7c368197ccc0f17b3c4ed6b7350c2e7e4
mb1a15 <mb4e10@soton.ac.uk>
2016-09-27 16:11:03 +0100

    Tidy up drivers code.

--- oommfc/drivers/mindriver.py
+++ oommfc/drivers/mindriver.py
@@ -4,26 +4,31 @@ from .driver import Driver
 
 class MinDriver(Driver):
     def script(self, system):
+        meshname = system.mesh.name
+        Ms = system.m.normalisedto
+        systemname = system.name
+        
         mif = "# CGEvolver\n"
         mif += "Specify Oxs_CGEvolve {}\n\n"
         mif += "# MinDriver\n"
         mif += "Specify Oxs_MinDriver {\n"
         mif += "  evolver Oxs_CGEvolve\n"
         mif += "  stopping_mxHxm 0.01\n"
-        mif += "  mesh :{}\n".format(system.mesh.name)
-        mif += "  Ms {}\n".format(system.m.normalisedto)
+        mif += "  mesh :{}\n".format(meshname)
+        mif += "  Ms {}\n".format(Ms)
         mif += "  m0 {\n"
         mif += "    Oxs_FileVectorField {\n"
         mif += "      atlas :atlas\n"
         mif += "      norm 1.0\n"
-        mif += "      file m0file.omf\n"
+        mif += "      file m0.omf\n"
         mif += "    }\n"
         mif += "  }\n"
-        mif += "  basename {}\n".format(system.name)
+        mif += "  basename {}\n".format(systemname)
         mif += "  vector_field_output_format {text %\#.8g}\n"
         mif += "}\n\n"
         mif += "Destination table mmArchive\n"
         mif += "Destination mags mmArchive\n\n"
         mif += "Schedule DataTable table Stage 1\n"
         mif += "Schedule Oxs_MinDriver::Spin mags Stage 1"
+
         return mif
--- oommfc/drivers/timedriver.py
+++ oommfc/drivers/timedriver.py
@@ -4,30 +4,37 @@ from .driver import Driver
 
 class TimeDriver(Driver):
     def script(self, system, **kwargs):
+        alpha = system.dynamics.damping.alpha
+        gamma = system.dynamics.precession.gamma
+        meshname = system.mesh.name
+        Ms = system.m.normalisedto
+        systemname = system.name
+
         mif = "# RungeKuttaEvolver\n"
         mif += "Specify Oxs_RungeKuttaEvolve:evolver {\n"
-        mif += "  alpha {}\n".format(system.dynamics.damping.alpha)
-        mif += "  gamma_G {}\n".format(system.dynamics.precession.gamma)
+        mif += "  alpha {}\n".format(alpha)
+        mif += "  gamma_G {}\n".format(gamma)
         mif += "}\n\n"
         mif += "# TimeDriver\n"
         mif += "Specify Oxs_TimeDriver {\n"
         mif += "  evolver Oxs_RungeKuttaEvolve\n"
         mif += "  stopping_time {}\n".format(kwargs["t"]/kwargs["n"])
-        mif += "  mesh :{}\n".format(system.mesh.name)
+        mif += "  mesh :{}\n".format(meshname)
         mif += "  stage_count {}\n".format(kwargs["n"])
-        mif += "  Ms {}\n".format(system.m.normalisedto)
+        mif += "  Ms {}\n".format(Ms)
         mif += "  m0 {\n"
         mif += "    Oxs_FileVectorField {\n"
         mif += "      atlas :atlas\n"
         mif += "      norm 1.0\n"
-        mif += "      file m0file.omf\n"
+        mif += "      file m0.omf\n"
         mif += "    }\n"
         mif += "  }\n"
-        mif += "  basename {}\n".format(system.name)
+        mif += "  basename {}\n".format(systemname)
         mif += "  vector_field_output_format {text %\#.8g}\n"
         mif += "}\n\n"
         mif += "Destination table mmArchive\n"
         mif += "Destination mags mmArchive\n\n"
         mif += "Schedule DataTable table Stage 1\n"
         mif += "Schedule Oxs_TimeDriver::Spin mags Stage 1"
+
         return mif
