dfaa542f06c2dcda856da609c3bd878f63943ac2
mb1a15 <mb4e10@soton.ac.uk>
2016-09-27 14:38:26 +0100

    Add all objects to oommfc imports.

--- oommfc/__init__.py
+++ oommfc/__init__.py
@@ -1,2 +1,6 @@
 from .system import System
 from .mesh import Mesh
+from .hamiltonian import Exchange, UniaxialAnisotropy, \
+    Demag, Zeeman, Hamiltonian
+from .dynamics import Precession, Damping, Dynamics
+from .drivers import MinDriver, TimeDriver
