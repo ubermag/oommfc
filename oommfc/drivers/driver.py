e529042fd55fb14f23629b9bbf5aee44219892d0
mb1a15 <mb4e10@soton.ac.uk>
2016-09-27 11:15:42 +0100

    Restructure drivers.

--- oommfc/drivers/driver.py
+++ oommfc/drivers/driver.py
@@ -3,28 +3,58 @@ from micromagneticmodel.drivers import Driver
 
 
 class Driver(Driver):
-    def drive(self):
-        self.dirname = self.system.name + '/'
-        if not os.path.exists(self.dirname):
-            os.makedirs(self.dirname)
-        self.mif_filename = self.dirname + self.system.name + '.mif'
+    def drive(self, system):
+        """
+        Drive the system using energy minimisation driver.
 
-        self.system.m.write_oommf_file(self.dirname+"m0file.omf")
+        """
+        # Make directory for saving OOMMF files.
+        dirname = "{}/".format(system.name)
+        self._makedirs(dirname)
 
-        miffile = open(self.mif_filename, 'w')
-        miffile.write(self.script())
+        # Save system's magnetisation configuration omf file.
+        omffilename = "{}m0file.omf".format(dirname)
+        system.m.write_oommf_file(omffilename)
+
+        miffilename = "{}{}.mif".format(dirname, system.name)
+        self.save_mif(system, miffilename)
+
+        # Run simulation.
+        self.run_simulator(dirname, miffilename)
+
+        # Update system.
+        self.update_system()
+
+    def _makedirs(self, dirname):
+        """
+        Create directory where OOMMF files are saved.
+        """
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+    def save_mif(self, system, miffilename):
+        """
+        Save OOMMF configuration mif file.
+        """
+        mif = "# MIF 2.1\n\n"
+        mif += system.script()
+        mif += self.script(system)
+        
+        miffile = open(miffilename, 'w')
+        miffile.write(mif)
         miffile.close()
 
-        self.run_simulator()
-
-    def run_simulator(self):
+    def run_simulator(self, dirname, miffilename):
         if os.name == 'nt':
             oommf_command = 'tclsh86 %OOMMFTCL% boxsi +fg '
         else:
             oommf_command = 'tclsh $OOMMFTCL boxsi +fg '
-        oommf_command += self.mif_filename
+        oommf_command += miffilename
         oommf_command += ' -exitondone 1'
-        os.system('cd ' + self.dirname)
+        os.system('cd ' + dirname)
         returncode = os.system(oommf_command)
         if returncode:
             raise Exception("Something has gone wrong in running OOMMF")
+
+    def update_system(self):
+        pass
--- oommfc/drivers/mindriver.py
+++ oommfc/drivers/mindriver.py
@@ -1,19 +1,16 @@
 import os
-from micromagneticmodel.drivers import MinDriver
+from .driver import Driver
 
 
-class MinDriver(MinDriver):
-    def script(self):
-        mif = "# MIF 2.1\n\n"
-        mif += self.system.mesh.script()
-        mif += self.system.hamiltonian.script()
-        mif += "Specify Oxs_CGEvolve {}\n\n"
+class MinDriver(Driver):
+    def script(self, system):
+        mif = "Specify Oxs_CGEvolve {}\n\n"
         mif += "# MinDriver\n"
         mif += "Specify Oxs_MinDriver {\n"
         mif += "  evolver Oxs_CGEvolve\n"
         mif += "  stopping_mxHxm 0.01\n"
-        mif += "  mesh :{}\n".format(self.system.mesh.name)
-        mif += "  Ms {}\n".format(self.system.m.normalisedto)
+        mif += "  mesh :{}\n".format(system.mesh.name)
+        mif += "  Ms {}\n".format(system.m.normalisedto)
         mif += "  m0 {\n"
         mif += "    Oxs_FileVectorField {\n"
         mif += "      atlas :atlas\n"
@@ -21,39 +18,12 @@ class MinDriver(MinDriver):
         mif += "      file m0file.omf\n"
         mif += "    }\n"
         mif += "  }\n"
-        mif += "  basename {}\n".format(self.system.name)
+        mif += "  basename {}\n".format(system.name)
         mif += "  vector_field_output_format {text %\#.8g}\n"
         mif += "}\n\n"
         mif += "Destination table mmArchive\n"
         mif += "Destination mags mmArchive\n\n"
         mif += "Schedule DataTable table Stage 1\n"
         mif += "Schedule Oxs_MinDriver::Spin mags Stage 1"
-
         return mif
-
-    def drive(self):
-        self.dirname = self.system.name + '/'
-        if not os.path.exists(self.dirname):
-            os.makedirs(self.dirname)
-        self.mif_filename = self.dirname + self.system.name + '.mif'
-
-        self.system.m.write_oommf_file(self.dirname+"m0file.omf")
-
-        miffile = open(self.mif_filename, 'w')
-        miffile.write(self.script())
-        miffile.close()
-
-        self.run_simulator()
-
-    def run_simulator(self):
-        if os.name == 'nt':
-            oommf_command = 'tclsh86 %OOMMFTCL% boxsi +fg '
-        else:
-            oommf_command = 'tclsh $OOMMFTCL boxsi +fg '
-        oommf_command += self.mif_filename
-        oommf_command += ' -exitondone 1'
-        os.system('cd ' + self.dirname)
-        returncode = os.system(oommf_command)
-        if returncode:
-            raise Exception("Something has gone wrong in running OOMMF")
 
