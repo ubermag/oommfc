8ab0a726b01bbd0fedcb2b0c3db8c92e7b9dc2a9
mb1a15 <mb4e10@soton.ac.uk>
2016-09-29 17:02:12 +0100

    Update version in setup.py.

--- setup.py
+++ setup.py
@@ -1,26 +1,22 @@
 from setuptools import setup
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
 setup(
-    name='oommfc',
-    version='0.4',
-    description='A Python-based OOMMF calculator.',
+    name="oommfc",
+    version="0.5.1",
+    description="A Python-based OOMMF calculator.",
     long_description=readme,
-    author='Computational Modelling Group',
-    author_email='fangohr@soton.ac.uk',
-    url='https://github.com/joommf/oommfc',
-    download_url='https://github.com/joommf/oommfc/tarball/0.4',
-    packages=['oommfc',
-              'oommfc.tests'],
-    install_requires=[
-        'discretisedfield',
-        'micromagneticmodel',
-        'oommfodt'
-    ],
-    classifiers=[
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
-    ]
+    author="Computational Modelling Group",
+    author_email="fangohr@soton.ac.uk",
+    url="https://github.com/joommf/oommfc",
+    download_url="https://github.com/joommf/oommfc/tarball/0.5.1",
+    packages=["oommfc",
+              "oommfc.tests"],
+    install_requires=["discretisedfield",
+                      "micromagneticmodel",
+                      "oommfodt"],
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 3"]
 )
