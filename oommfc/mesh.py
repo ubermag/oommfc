ea0dbb21229fdcbb97a45eff7acf156ae3148845
mb1a15 <mb4e10@soton.ac.uk>
2016-10-03 13:21:53 +0100

    Changed mesh arguments.

--- oommfc/mesh.py
+++ oommfc/mesh.py
@@ -6,15 +6,15 @@ class Mesh(df.Mesh):
     def script(self):
         mif = "# BoxAtlas\n"
         mif += "Specify Oxs_BoxAtlas:atlas {\n"
-        mif += "  xrange {{{} {}}}\n".format(self.c1[0], self.c2[0])
-        mif += "  yrange {{{} {}}}\n".format(self.c1[1], self.c2[1])
-        mif += "  zrange {{{} {}}}\n".format(self.c1[2], self.c2[2])
+        mif += "  xrange {{{} {}}}\n".format(self.p1[0], self.p2[0])
+        mif += "  yrange {{{} {}}}\n".format(self.p1[1], self.p2[1])
+        mif += "  zrange {{{} {}}}\n".format(self.p1[2], self.p2[2])
         mif += "}\n\n"
         mif += "# RectangularMesh\n"
         mif += "Specify Oxs_RectangularMesh:{} {{\n".format(self.name)
-        mif += "  cellsize {{{} {} {}}}\n".format(self.d[0],
-                                                  self.d[1],
-                                                  self.d[2])
+        mif += "  cellsize {{{} {} {}}}\n".format(self.cell[0],
+                                                  self.cell[1],
+                                                  self.cell[2])
         mif += "  atlas Oxs_BoxAtlas:atlas\n"
         mif += "}\n\n"
 
--- oommfc/tests/test_mesh.py
+++ oommfc/tests/test_mesh.py
@@ -5,12 +5,12 @@ import discretisedfield.tests as dft
 class TestMesh(dft.TestMesh):
     def test_get_mif(self):
         for arg in self.valid_args:
-            c1 = arg[0]
-            c2 = arg[1]
-            d = arg[2]
+            p1 = arg[0]
+            p2 = arg[1]
+            cell = arg[2]
             name = "test_mesh"
 
-            mesh = oc.Mesh(c1, c2, d, name=name)
+            mesh = oc.Mesh(p1, p2, cell, name=name)
 
             script = mesh.script()
             assert script.count("\n") == 13
@@ -23,9 +23,9 @@ class TestMesh(dft.TestMesh):
             # Assert BoxAtlas script
             assert lines[0] == "# BoxAtlas"
             assert lines[1] == "Specify Oxs_BoxAtlas:atlas {"
-            assert lines[2] == "  xrange {{{} {}}}".format(c1[0], c2[0])
-            assert lines[3] == "  yrange {{{} {}}}".format(c1[1], c2[1])
-            assert lines[4] == "  zrange {{{} {}}}".format(c1[2], c2[2])
+            assert lines[2] == "  xrange {{{} {}}}".format(p1[0], p2[0])
+            assert lines[3] == "  yrange {{{} {}}}".format(p1[1], p2[1])
+            assert lines[4] == "  zrange {{{} {}}}".format(p1[2], p2[2])
             assert lines[5] == "}"
 
             # Empty line between BoxAtlas and RectangularMesh
@@ -34,8 +34,8 @@ class TestMesh(dft.TestMesh):
             # Assert RectangularMesh script
             assert lines[7] == "# RectangularMesh"
             assert lines[8] == "Specify Oxs_RectangularMesh:{} {{".format(name)
-            assert lines[9] == "  cellsize {{{} {} {}}}".format(d[0],
-                                                                d[1],
-                                                                d[2])
+            assert lines[9] == "  cellsize {{{} {} {}}}".format(cell[0],
+                                                                cell[1],
+                                                                cell[2])
             assert lines[10] == "  atlas Oxs_BoxAtlas:atlas"
             assert lines[11] == "}"
