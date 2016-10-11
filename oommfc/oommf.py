411b410b2280dbfe20288bfd3cdef199c3d11a68
mb1a15 <mb4e10@soton.ac.uk>
2016-10-11 13:10:49 +0100

    Add more methods to the OOMMF class.

--- oommfc/oommf.py
+++ oommfc/oommf.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 
@@ -9,3 +10,23 @@ class OOMMF:
             return False
         else:
             return True
+
+    def environment_variable(self, varname="OOMMFTCL"):
+        if not os.getenv(varname, False):
+            return False
+        else:
+            return True
+
+    def test_oommf(self):
+        if not self.installed():
+            raise Exception("OOMMF is not installed.")
+        elif not self.environment_variable():
+            raise Exception("OOMMFTCL environment variable not set.")
+        else:
+            return True
+
+    def version(self):
+        if self.test_oommf():
+            output = subprocess.check_output(["tclsh", "$OOMMFTCL", "+version"])
+            return output
+            
--- oommfc/tests/test_oommf.py
+++ oommfc/tests/test_oommf.py
@@ -2,9 +2,20 @@ import oommfc as oc
 
 
 class TestOOMMF:
-    def test_installed(self):
-        oommf = oc.OOMMF()
+    def setup(self):
+        self.oommf = oc.OOMMF()
 
-        assert oommf.installed() is True
-        assert oommf.installed("oommf") is True
-        assert oommf.installed("OOMMF") is False
+    def test_installed(self):
+        assert self.oommf.installed() is True
+        assert self.oommf.installed("oommf") is True
+        assert self.oommf.installed("OOMMF") is False
+
+    def test_environment_variable(self):
+        assert self.oommf.environment_variable("OOMMFTCL") is True
+        assert self.oommf.environment_variable("OOMMFTCL2") is False
+
+    def test_test_oommf(self):
+        assert self.oommf.test_oommf() is True
+
+    def test_version(self):
+        assert self.oommf.test_oommf() is True
