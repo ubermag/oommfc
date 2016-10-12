61736cc0b7476053eb188161c753fa7ef8e5bfbc
mb1a15 <mb4e10@soton.ac.uk>
2016-10-12 13:18:20 +0100

    Update standard problem 3 (still needs polishing).

--- oommfc/system.py
+++ oommfc/system.py
@@ -6,3 +6,8 @@ class System(mm.System):
         mif = self.mesh.script()
         mif += self.hamiltonian.script()
         return mif
+
+    def total_energy(self):
+        for key in self.dt.tail(1).keys():
+            if "Totalenergy" in key:
+                return self.dt.tail(1)[key][0]
