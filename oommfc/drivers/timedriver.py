5c55009a063672a885422f433eb89bed613f5746
mb1a15 <mb4e10@soton.ac.uk>
2016-10-12 15:43:24 +0100

    Extend time driver for STT.

--- oommfc/drivers/timedriver.py
+++ oommfc/drivers/timedriver.py
@@ -3,20 +3,52 @@ from .driver import Driver
 
 class TimeDriver(Driver):
     def script(self, system, **kwargs):
-        alpha = system.dynamics.damping.alpha
-        gamma = system.dynamics.precession.gamma
+        try:
+            alpha = system.dynamics.damping.alpha
+        except AttributeError:
+            alpha = 0
+        try:
+            gamma = system.dynamics.precession.gamma
+        except AttributeError:
+            gamma = 2.211e5
+        try:
+            u = system.dynamics.stt.u
+            beta = system.dynamics.stt.beta
+        except AttributeError:
+            stt = False
+        else:
+            stt = True
+
         meshname = system.mesh.name
         Ms = system.m.normalisedto
         systemname = system.name
 
-        mif = "# RungeKuttaEvolver\n"
-        mif += "Specify Oxs_RungeKuttaEvolve:evolver {\n"
-        mif += "  alpha {}\n".format(alpha)
-        mif += "  gamma_G {}\n".format(gamma)
-        mif += "}\n\n"
+        if not stt:
+            mif = "# RungeKuttaEvolver\n"
+            mif += "Specify Oxs_RungeKuttaEvolve:evolver {\n"
+            mif += "  alpha {}\n".format(alpha)
+            mif += "  gamma_G {}\n".format(gamma)
+            mif += "}\n\n"
+            evolver = "Oxs_RungeKuttaEvolve"
+        else:
+            mif = "# SpinTransferTorqueEvolver\n"
+            mif += "Specify Anv_SpinTEvolve {\n"
+            mif += "  do_precess 1\n"
+            mif += "  gamma_G {}\n".format(gamma)
+            mif += "  alpha {}\n".format(alpha)
+            mif += "  method rkf54s\n"
+            mif += "  u {\n"
+            mif += "    Oxs_UniformScalarField {\n"
+            mif += "      value {}\n".format(u[0])
+            mif += "    }\n"
+            mif += "  }\n"
+            mif += "  beta {}\n".format(beta)
+            mif += "}\n\n"
+            evolver = "Anv_SpinTEvolve"
+
         mif += "# TimeDriver\n"
         mif += "Specify Oxs_TimeDriver {\n"
-        mif += "  evolver Oxs_RungeKuttaEvolve\n"
+        mif += "  evolver {}\n".format(evolver)
         mif += "  stopping_time {}\n".format(kwargs["t"]/kwargs["n"])
         mif += "  mesh :{}\n".format(meshname)
         mif += "  stage_count {}\n".format(kwargs["n"])
