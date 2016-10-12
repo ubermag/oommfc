78d01081950fe6dd78d13d44e094d640cd802149
mb1a15 <mb4e10@soton.ac.uk>
2016-10-12 14:11:59 +0100

    Add hysteresis driver.

--- oommfc/__init__.py
+++ oommfc/__init__.py
@@ -2,7 +2,7 @@ from .oommf import OOMMF
 from .hamiltonian import Exchange, UniaxialAnisotropy, \
     Demag, Zeeman, Hamiltonian
 from .dynamics import Precession, Damping, Dynamics
-from .drivers import Driver, MinDriver, TimeDriver
+from .drivers import Driver, MinDriver, TimeDriver, HysteresisDriver
 from .mesh import Mesh
 from .system import System
 from micromagneticmodel.consts import mu0
