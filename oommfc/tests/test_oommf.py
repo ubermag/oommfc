db112e3f064b02308f495a14aeb82c26eb16b67d
mb1a15 <mb4e10@soton.ac.uk>
2016-10-18 14:00:16 +0100

    Remove docker_available test on Travis.

--- oommfc/tests/test_oommf.py
+++ oommfc/tests/test_oommf.py
@@ -7,7 +7,3 @@ class TestOOMMF:
         oommf = oc.OOMMF()
         with pytest.raises(EnvironmentError):
             oommf.getenv("NONEXISTINGENV123")
-
-    def test_docker_available(self):
-        oommf = oc.OOMMF()
-        assert isinstance(oommf.docker_available(), bool)
