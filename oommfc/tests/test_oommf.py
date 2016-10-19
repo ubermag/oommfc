daaf3428d034fb567ba5231e6b2ebc710306c9b9
mb1a15 <mb4e10@soton.ac.uk>
2016-10-19 12:02:27 +0100

    Remove oommf class tests temporarily.

--- oommfc/tests/test_oommf.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import oommfc as oc
-
-
-class TestOOMMF:
-    def test_getenv(self):
-        oommf = oc.OOMMF()
-        with pytest.raises(EnvironmentError):
-            oommf.getenv("NONEXISTINGENV123")
