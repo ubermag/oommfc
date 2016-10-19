55063f0d13341477168d0954490592395ccecb10
mb1a15 <mb4e10@soton.ac.uk>
2016-10-19 14:32:08 +0100

    Extend oommf tests.

--- oommfc/tests/test_oommf.py
+++ oommfc/tests/test_oommf.py
@@ -7,3 +7,10 @@ class TestOOMMF:
 
         status = oommf.status()
         assert isinstance(status, dict)
+
+    def test_call_oommf(self):
+        oommf = oc.OOMMF()
+
+        oommf.call_oommf(argstr="+v", where=None)
+        oommf.call_oommf(argstr="+v", where="host")
+        oommf.call_oommf(argstr="+v", where="docker")
