0cd8dd47dbe71b932505c1009a352c90dfa0df08
mb1a15 <mb4e10@soton.ac.uk>
2016-10-27 14:52:29 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name='oommfc',
-    version='0.5.6.3',
+    version='0.5.6.4',
     description='A Python-based OOMMF calculator.',
     long_description=readme,
     url='https://joommf.github.io',
