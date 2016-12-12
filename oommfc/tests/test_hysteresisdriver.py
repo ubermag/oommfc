1dc12adaaa9b5e8d053f06794e858c58e2b9500c
mb1a15 <m.beg@soton.ac.uk>
2016-12-12 14:06:35 +0000

    Extend hysteresisdriver tests.

--- oommfc/tests/test_hysteresisdriver.py
+++ oommfc/tests/test_hysteresisdriver.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import shutil
+import pytest
 import oommfc as oc
 from .test_driver import TestDriver
 
@@ -24,3 +26,40 @@ class TestHysteresisDriver(TestDriver):
         assert "Oxs_CGEvolve" in script
         assert "Oxs_MinDriver" in script
         assert "Oxs_FileVectorField" in script
+
+    def test_save_mif(self):
+        hd = oc.HysteresisDriver()
+
+        hd._makedir(self.system)
+        hd._save_mif(self.system, Hmin=(0, 0, 0), Hmax=(1, 1, 1), n=10)
+
+        miffilename = os.path.join("tds", "tds.mif")
+        assert os.path.isfile(miffilename)
+
+        lines = open(miffilename, "r").readlines()
+        assert lines[0] == "# MIF 2.1\n"
+        assert lines[-1][-1] == "1"
+
+        shutil.rmtree("tds")
+
+    @pytest.mark.oommf
+    def test_drive(self):
+        hd = oc.HysteresisDriver()
+
+        hd.drive(self.system, Hmin=(0, 0, 0), Hmax=(1, 1, 1), n=10)
+
+        dirname = os.path.join("tds", "")
+        assert os.path.exists(dirname)
+        miffilename = os.path.join("tds", "tds.mif")
+        assert os.path.isfile(miffilename)
+
+        omf_files = list(glob.iglob("tds/*.omf"))
+        odt_files = list(glob.iglob("tds/*.odt"))
+
+        assert len(omf_files) == 22
+        omffilename = os.path.join("tds", "m0.omf")
+        assert omffilename in omf_files
+
+        assert len(odt_files) == 1
+
+        shutil.rmtree("tds")
