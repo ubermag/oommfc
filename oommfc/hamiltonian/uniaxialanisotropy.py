9427eedfbd1182e5a56dd3a602a7e207a1bd170f
mb1a15 <m.beg@soton.ac.uk>
2016-12-12 14:11:48 +0000

    Simplify uniaxialanisotropy script code.

--- oommfc/hamiltonian/uniaxialanisotropy.py
+++ oommfc/hamiltonian/uniaxialanisotropy.py
@@ -6,7 +6,7 @@ class UniaxialAnisotropy(mm.UniaxialAnisotropy):
         mif = "# UniaxialAnisotropy\n"
         mif += "Specify Oxs_UniaxialAnisotropy {\n"
         mif += "  K1 {}\n".format(self.K)
-        mif += "  axis {{{} {} {}}}\n".format(self.u[0], self.u[1], self.u[2])
+        mif += "  axis {{{} {} {}}}\n".format(*self.u[0])
         mif += "}\n\n"
 
         return mif
