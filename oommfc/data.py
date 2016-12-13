5ac0fed82b2e681accdc210728e73bee57226faa
mb1a15 <m.beg@soton.ac.uk>
2016-12-13 15:42:39 +0000

    Extend data

--- oommfc/data.py
+++ oommfc/data.py
@@ -6,20 +6,23 @@ import micromagneticmodel as mm
 
 
 class Data(mm.Data):
-    _dict = {"Demag": "Oxs_Demag",
-             "Exchange": "Oxs_UniformExchange",
-             "UniaxialAnisotropy": "Oxs_UniaxialAnisotropy",
-             "Zeeman": "Oxs_FixedZeeman"}
     @property
     def effective_field(self):
-        
-        derive = "{}::Field".format(self._dict[self.interaction])
+        _dict = {"Demag": "Oxs_Demag::Field",
+                 "Exchange": "Oxs_UniformExchange::Field",
+                 "UniaxialAnisotropy": "Oxs_UniaxialAnisotropy::Field",
+                 "Zeeman": "Oxs_FixedZeeman::Field",
+                 "Hamiltonian": "Oxs_RungeKuttaEvolve:evolver:Total field"}
 
         td = oc.TimeDriver()
-        td.drive(self.system, derive=derive)
+        td.drive(self.system, derive=_dict[self.interaction])
 
         dirname = os.path.join(self.system.name, "")
         last_ohf_file = max(glob.iglob("{}*.ohf".format(dirname)),
                             key=os.path.getctime)
 
         return df.read_oommf_file(last_ohf_file)
+
+    @property
+    def energy(self):
+        pass
