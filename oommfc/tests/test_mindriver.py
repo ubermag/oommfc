24f570525dbdbf3bd9990eca042f0fb33b6df064
mb1a15 <m.beg@soton.ac.uk>
2016-12-14 14:09:39 +0000

    Update mindriver tests.

--- oommfc/tests/test_mindriver.py
+++ oommfc/tests/test_mindriver.py
@@ -14,8 +14,8 @@ class TestMinDriver(TestDriver):
 
         assert script[0] == "#"
         assert script[-1] == "1"
-        assert script.count("#") == 3
-        assert script.count("Specify") == 2
+        assert script.count("#") == 4
+        assert script.count("Specify") == 3
         assert script.count("Destination") == 2
         assert script.count("Schedule") == 2
         assert script.count("mmArchive") == 2
@@ -24,11 +24,6 @@ class TestMinDriver(TestDriver):
         assert "Oxs_CGEvolve" in script
         assert "Oxs_MinDriver" in script
         assert "Oxs_FileVectorField" in script
-
-        lines = script.split("\n")
-        assert lines[8] == "  Ms {}".format(8e5)
-        assert lines[13] == "      file m0.omf"
-        assert lines[16] == "  basename tds"
 
     def test_save_mif(self):
         md = oc.MinDriver()
