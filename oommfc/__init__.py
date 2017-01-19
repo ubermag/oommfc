0701ffbc2ee308d3de9092102a3900c411c6d699
Hans Fangohr <fangohr@soton.ac.uk>
2017-01-19 21:01:06 +0000

    Adding example. Needs documentation.
    oommfc.examples.bar() creates an system, comparable to the nmag 'bar' example.
    
    [We may also want to get an easier way of creating a time driver.]

--- oommfc/__init__.py
+++ oommfc/__init__.py
@@ -1,38 +1,30 @@
-from .oommf import OOMMF
+import oommfc.oommf
 from .hamiltonian import Exchange, UniaxialAnisotropy, \
     Demag, Zeeman, Hamiltonian
 from .dynamics import Precession, Damping, STT, Dynamics
 from .drivers import Driver, MinDriver, TimeDriver, HysteresisDriver
 from .mesh import Mesh
 from .system import System
-from .data import Data
 from micromagneticmodel.consts import mu0, e, me, kB, h, g, \
     hbar, gamma, muB, gamma0
-
-oommf = OOMMF()
+import oommfc.examples
 
 
 def test():
     """Runs all the tests"""
-    oommf.status(raise_exception=True)  # pragma: no cover
+    oommfc.oommf.status(raise_exception=True)  # pragma: no cover
     import pytest  # pragma: no cover
-    args = ["-m", "not travis", "-v", "--pyargs",
-            "oommfc"]  # pragma: no cover
-    pytest.main(args)  # pragma: no cover
+    pytest.main(["-v", "--pyargs", "oommfc"])  # pragma: no cover
 
 
-def test_not_oommf():
+def test_non_oommf():
     """ Runs tests that do not need an OOMMF installation"""
     import pytest  # pragma: no cover
-    args = ["-m", "not travis", "-m", "not oommf", "-v",
-            "--pyargs", "oommfc"]  # pragma: no cover
-    pytest.main(args)  # pragma: no cover
+    pytest.main(["-m", "not oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
 
 
 def test_oommf():
     """Runs all tests that require an OOMMF installation."""
-    oommf.status(raise_exception=True)  # pragma: no cover
+    oommfc.oommf.status(raise_exception=True)  # pragma: no cover
     import pytest  # pragma: no cover
-    args = ["-m", "oommf and not travis", "-v",
-            "--pyargs", "oommfc"]  # pragma: no cover
-    pytest.main(args)  # pragma: no cover
+    pytest.main(["-m", "oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
