59e95a54e0b9bc3a43f129b3cd1b3abb4de2df19
Hans Fangohr <fangohr@soton.ac.uk>
2017-01-19 21:25:04 +0000

    Undo accidental commit for this file.

--- oommfc/__init__.py
+++ oommfc/__init__.py
@@ -1,30 +1,38 @@
-import oommfc.oommf
+from .oommf import OOMMF
 from .hamiltonian import Exchange, UniaxialAnisotropy, \
     Demag, Zeeman, Hamiltonian
 from .dynamics import Precession, Damping, STT, Dynamics
 from .drivers import Driver, MinDriver, TimeDriver, HysteresisDriver
 from .mesh import Mesh
 from .system import System
+from .data import Data
 from micromagneticmodel.consts import mu0, e, me, kB, h, g, \
     hbar, gamma, muB, gamma0
-import oommfc.examples
+
+oommf = OOMMF()
 
 
 def test():
     """Runs all the tests"""
-    oommfc.oommf.status(raise_exception=True)  # pragma: no cover
+    oommf.status(raise_exception=True)  # pragma: no cover
     import pytest  # pragma: no cover
-    pytest.main(["-v", "--pyargs", "oommfc"])  # pragma: no cover
+    args = ["-m", "not travis", "-v", "--pyargs",
+            "oommfc"]  # pragma: no cover
+    pytest.main(args)  # pragma: no cover
 
 
-def test_non_oommf():
+def test_not_oommf():
     """ Runs tests that do not need an OOMMF installation"""
     import pytest  # pragma: no cover
-    pytest.main(["-m", "not oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
+    args = ["-m", "not travis", "-m", "not oommf", "-v",
+            "--pyargs", "oommfc"]  # pragma: no cover
+    pytest.main(args)  # pragma: no cover
 
 
 def test_oommf():
     """Runs all tests that require an OOMMF installation."""
-    oommfc.oommf.status(raise_exception=True)  # pragma: no cover
+    oommf.status(raise_exception=True)  # pragma: no cover
     import pytest  # pragma: no cover
-    pytest.main(["-m", "oommf", "-v", "--pyargs", "oommfc"])  # pragma: no cover
+    args = ["-m", "oommf and not travis", "-v",
+            "--pyargs", "oommfc"]  # pragma: no cover
+    pytest.main(args)  # pragma: no cover
