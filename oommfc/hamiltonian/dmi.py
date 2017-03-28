c850b4a6fa1be875841492eca64fcba53b8d2623
mb1a15 <m.beg@soton.ac.uk>
2017-03-28 12:11:44 +0100

    Add interfacial DMI script.

--- oommfc/hamiltonian/dmi.py
+++ oommfc/hamiltonian/dmi.py
@@ -4,8 +4,12 @@ import micromagneticmodel as mm
 class DMI(mm.DMI):
     @property
     def _script(self):
-        mif = "# BulkDMI\n"
-        mif += "Specify Oxs_BulkDMI {\n"
+        if self.kind == "bulk":
+            mif = "# BulkDMI\n"
+            mif += "Specify Oxs_BulkDMI {\n"
+        elif self.kind == "interfacial":
+            mif = "# InterfacialDMI\n"
+            mif += "Specify Oxs_DMExchange6Ngbr {\n"
         mif += "  default_D {}\n".format(self.D)
         mif += "  atlas :atlas\n"
         mif += "  D {\n"
