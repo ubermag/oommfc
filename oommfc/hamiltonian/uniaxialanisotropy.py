098cb11b6a64f4ce626c5059c2963df292b834af
mb1a15 <m.beg@soton.ac.uk>
2017-05-03 16:28:34 +0100

    Add higher order anisotropy.

--- oommfc/hamiltonian/uniaxialanisotropy.py
+++ oommfc/hamiltonian/uniaxialanisotropy.py
@@ -5,9 +5,16 @@ class UniaxialAnisotropy(mm.UniaxialAnisotropy):
     @property
     def _script(self):
         mif = "# UniaxialAnisotropy\n"
-        mif += "Specify Oxs_UniaxialAnisotropy {\n"
-        mif += "  K1 {}\n".format(self.K1)
-        mif += "  axis {{{} {} {}}}\n".format(*self.u)
-        mif += "}\n\n"
+        if self.K2 == 0:
+            mif += "Specify Oxs_UniaxialAnisotropy {\n"
+            mif += "  K1 {}\n".format(self.K1)
+            mif += "  axis {{{} {} {}}}\n".format(*self.u)
+            mif += "}\n\n"
+        else:
+            mif += "Specify Southampton_UniaxialAnisotropy4 {\n"
+            mif += "  K1 {}\n".format(self.K1)
+            mif += "  K2 {}\n".format(self.K2)
+            mif += "  axis {{{} {} {}}}\n".format(*self.u)
+            mif += "}\n\n"
 
         return mif
--- oommfc/tests/test_uniaxialanisotropy.py
+++ oommfc/tests/test_uniaxialanisotropy.py
@@ -5,19 +5,25 @@ import micromagneticmodel.tests as mmt
 class TestUniaxialAnisotropy(mmt.TestUniaxialAnisotropy):
     def test_script(self):
         for K1, K2, u in self.valid_args:
-            name = "anisotropy_test"
-
-            anisotropy = oc.UniaxialAnisotropy(K1=K1, u=u, name=name)
-
+            anisotropy = oc.UniaxialAnisotropy(K1=K1, K2=K2, u=u)
             script = anisotropy._script
-            assert script.count("\n") == 6
+
             assert script[0] == "#"
             assert script[-1] == "\n"
-
             lines = script.split("\n")
-            assert len(lines) == 7
             assert lines[0] == "# UniaxialAnisotropy"
-            assert lines[1] == "Specify Oxs_UniaxialAnisotropy {"
             assert lines[2] == "  K1 {}".format(K1)
-            assert lines[3] == "  axis {{{} {} {}}}".format(*u)
-            assert lines[4] == "}"
+            assert lines[-4] == "  axis {{{} {} {}}}".format(*u)
+            assert lines[-3] == "}"
+            
+            if K2 == 0:
+                assert script.count("\n") == 6
+                assert len(lines) == 7
+                assert lines[1] == "Specify Oxs_UniaxialAnisotropy {"
+            else:
+                assert script.count("\n") == 7
+                assert len(lines) == 8
+                assert lines[1] == "Specify Southampton_UniaxialAnisotropy4 {"
+                assert lines[3] == "  K2 {}".format(K2)
+
+
