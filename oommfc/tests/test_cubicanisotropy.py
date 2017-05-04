112102c1a87cdf191b2da40882f6da2d0005ff9c
mb1a15 <m.beg@soton.ac.uk>
2017-05-04 11:04:15 +0100

    Extend cubic anisotropy test.

--- oommfc/tests/test_cubicanisotropy.py
+++ oommfc/tests/test_cubicanisotropy.py
@@ -1,4 +1,7 @@
+import os
+import shutil
 import oommfc as oc
+import discretisedfield as df
 import micromagneticmodel.tests as mmt
 
 
@@ -18,3 +21,42 @@ class TestCubicAnisotropy(mmt.TestCubicAnisotropy):
             assert lines[3] == "  axis1 {{{} {} {}}}".format(*u1)
             assert lines[4] == "  axis2 {{{} {} {}}}".format(*u2)
             assert lines[5] == "}"
+
+
+def test_relax_with_cubicanisotropy():
+    name = "cubic_anisotropy"
+    L = 100e-9
+    d = 5e-9
+    Ms = 8e6
+
+    # Remove any previous simulation directories.
+    if os.path.exists(name):
+        shutil.rmtree(name)
+
+    system = oc.System(name=name)
+    system.hamiltonian = oc.CubicAnisotropy(K1=5e6,
+                                            u1=(1, 0, 0),
+                                            u2=(0, 1, 0))
+
+    mesh = oc.Mesh(p1=(0, 0, 0), p2=(L, L, L), cell=(d, d, d))
+
+    def m_init(pos):
+        x, y, z = pos
+        if x < 30e-9:
+            return (0.7, 0.1, 0.3)
+        elif x > 70e-9:
+            return (0.1, 0.7, 0.3)
+        else:
+            return (0.3, 0.1, 0.7)
+
+    system.m = df.Field(mesh, value=m_init, norm=Ms)
+
+    md = oc.MinDriver()
+    md.drive(system)
+
+    comp_value = 0.99*Ms
+    assert system.m((10e-9, 0, 0))[0] > comp_value
+    assert system.m((50e-9, 0, 0))[2] > comp_value
+    assert system.m((80e-9, 0, 0))[1] > comp_value
+
+    shutil.rmtree(name)
