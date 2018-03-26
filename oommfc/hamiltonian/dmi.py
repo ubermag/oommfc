c027d61d2174500ee29fcfb0e4209fdbfc1397c3
Marijan Beg <marijan.beg@xfel.eu>
2018-03-26 16:20:14 +0200

    Extend script property for different DMI classes.

--- oommfc/hamiltonian/dmi.py
+++ oommfc/hamiltonian/dmi.py
@@ -4,9 +4,15 @@ import micromagneticmodel as mm
 class DMI(mm.DMI):
     @property
     def _script(self):
-        if self.kind == "bulk":
-            mif = "# BulkDMI\n"
-            mif += "Specify Oxs_BulkDMI {\n"
+        if self.crystalclass in ["t", "o"]:
+            mif = "# DMI of crystallographic class T(O)\n"
+            mif += "Specify Oxs_DMI_T {\n"
+        elif self.crystalclass == "cnv":
+            mif = "# DMI of crystallographic class Cnv\n"
+            mif += "Specify Oxs_DMI_Cnv {\n"
+        elif self.crystalclass == "d2d":
+            mif = "# DMI of crystallographic class D2d\n"
+            mif += "Specify Oxs_DMI_D2d {\n"
         elif self.kind == "interfacial":
             mif = "# InterfacialDMI\n"
             mif += "Specify Oxs_DMExchange6Ngbr {\n"
