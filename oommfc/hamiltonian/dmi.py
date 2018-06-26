ef47986c8c9fda6dbc2983b321d5a4e8c35d5ac8
Marijan Beg <marijan.beg@xfel.eu>
2018-06-26 11:32:30 +0200

    Make conditions for different types of DMI on different platforms.

--- oommfc/hamiltonian/dmi.py
+++ oommfc/hamiltonian/dmi.py
@@ -1,21 +1,25 @@
+import sys
 import micromagneticmodel as mm
 
 
 class DMI(mm.DMI):
     @property
     def _script(self):
-        if self.crystalclass in ["t", "o"]:
+        if self.crystalclass in ["t", "o"] and sys.platform != "win32":
             mif = "# DMI of crystallographic class T(O)\n"
             mif += "Specify Oxs_DMI_T {\n"
-        elif self.crystalclass == "cnv":
+        elif self.crystalclass == "cnv" and sys.platform != "win32":
             mif = "# DMI of crystallographic class Cnv\n"
             mif += "Specify Oxs_DMI_Cnv {\n"
-        elif self.crystalclass == "d2d":
+        elif self.crystalclass == "d2d" and sys.platform != "win32":
             mif = "# DMI of crystallographic class D2d\n"
             mif += "Specify Oxs_DMI_D2d {\n"
-        elif self.crystalclass == "interfacial":
+        elif self.crystalclass == "cnv" and sys.platform == "win32":
             mif = "# InterfacialDMI\n"
             mif += "Specify Oxs_DMExchange6Ngbr {\n"
+        else:
+            raise ValueError("The {} crystal class is not supported "
+                             "on {} pltform.".format(self.crystalclass, sys.platform))
         mif += "  default_D {}\n".format(self.D)
         mif += "  atlas :atlas\n"
         mif += "  D {\n"
