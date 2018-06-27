8cb5f4ffcbf9d7d2634cfc51b17d5e46c1ea6e01
Marijan Beg <marijan.beg@xfel.eu>
2018-06-27 15:02:03 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name='oommfc',
-    version="0.8.2",
+    version="0.8.3",
     description='A Python-based OOMMF calculator.',
     long_description=readme,
     url='https://joommf.github.io',
