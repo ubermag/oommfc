394e6a295899c44bd93932ed826837f070f18c49
Marijan Beg <m.beg@soton.ac.uk>
2018-06-30 19:50:32 +0200

    Update setup.py for Markdown README.

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 import setuptools
 
-with open("README.md", 'r+', encoding="utf-8") as f:
-    readme = f.read()
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='oommfc',
-    version="0.8.4",
+    version='0.8.4',
     description='Python interface to OOMMF integrated into Jupyter notebook.',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://joommf.github.io',
     author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
     author_email='jupyteroommf@gmail.com',
     packages=setuptools.find_packages(),
     install_requires=['scipy',
-                      'discretisedfield',
                       'micromagneticmodel',
                       'oommfodt'],
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: OSI Approved :: BSD License',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
                  'Topic :: Scientific/Engineering :: Physics',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 3 :: Only']
+                 'Natural Language :: English']
 )
