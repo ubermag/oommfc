c03a20dd17dd460e3477a411aab78a4eebcbae13
Marijan Beg <marijan.beg@xfel.eu>
2018-07-05 21:55:00 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='oommfc',
-    version='0.8.6',
+    version='0.8.7',
     description='Python interface to OOMMF integrated into Jupyter notebook.',
     long_description=long_description,
     long_description_content_type='text/markdown',
