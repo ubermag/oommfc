f6e1897424a1bada430959a948fe4ab466c50573
Marijan Beg <marijan.beg@xfel.eu>
2018-10-26 14:12:08 +0200

    Start implementing directory overwrite.

--- oommfc/drivers/driver.py
+++ oommfc/drivers/driver.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import json
+import shutil
 import datetime
 import oommfc as oc
 import oommfodt as oo
@@ -9,7 +10,7 @@ import micromagneticmodel as mm
 
 
 class Driver(mm.Driver):
-    def drive(self, system, **kwargs):
+    def drive(self, system, overwrite=False, **kwargs):
         # This method is implemented in the derived class (TimeDriver,
         # MinDriver,...).
         self._check_args(**kwargs)
@@ -18,11 +19,16 @@ class Driver(mm.Driver):
         self.dirname = os.path.join(system.name,
                                     'drive-{}'.format(system.drive_number))
 
-        self.omffilename = os.path.join(self.dirname, "m0.omf")
+        self.omffilename = os.path.join(self.dirname, 'm0.omf')
         self.miffilename = os.path.join(self.dirname,
-                                        "{}.mif".format(system.name))
-        self.jsonfilename = os.path.join(self.dirname, "info.json")
+                                        '{}.mif'.format(system.name))
+        self.jsonfilename = os.path.join(self.dirname, 'info.json')
 
+        # Check whether a directory with the same name as system.name
+        # already exists. If it does, warn the user and tell him that
+        # he should pass overwrite=True to the drive method.
+        self._checkdir(system, overwrite=overwrite)
+        
         # Make a directory inside which OOMMF will be run.
         self._makedir()
 
@@ -40,23 +46,34 @@ class Driver(mm.Driver):
 
         # Update system's m and dt attributes if the derivation of E,
         # Heff, or energy density was not asked.
-        if "derive" not in kwargs:
+        if 'derive' not in kwargs:
             self._update_m(system)
             self._update_dt(system)
 
         # Increase the system's drive_number counter.
         system.drive_number += 1
 
+
+    def _checkdir(self, system, overwrite=False):
+        if os.path.exists(system.name):
+            if not overwrite:
+                msg = ('Directory with name={} already exists. If you want to overwrite it, '
+                       'pass overwrite=True to the drive method. Otherwise, change the name ',
+                       'of the system or delete the directory by running system.delete().').format(system.name)
+                raise FileExistsError(msg)
+            else:
+                shutil.rmtree(system.name)
+
     def _makedir(self):
         if not os.path.exists(self.dirname):
             os.makedirs(self.dirname)
 
     def _makemif(self, system, **kwargs):
-        mif = "# MIF 2.1\n\n"
+        mif = '# MIF 2.1\n\n'
         mif += system._script
         mif += self._script(system, **kwargs)
 
-        with open(self.miffilename, "w") as miffile:
+        with open(self.miffilename, 'w') as miffile:
             miffile.write(mif)
 
     def _makeomf(self, system):
@@ -67,7 +84,7 @@ class Driver(mm.Driver):
         info['date'] = datetime.datetime.now().strftime('%Y-%m-%d')
         info['time'] = datetime.datetime.now().strftime('%H:%M:%S')
 
-        with open(self.jsonfilename, "w") as jsonfile:
+        with open(self.jsonfilename, 'w') as jsonfile:
             jsonfile.write(json.dumps(info))
 
     def _run_oommf(self):
