ee36a13bc6154fbf5d32e37beed40df01f3f4338
Marijan Beg <marijan.beg@xfel.eu>
2019-08-17 21:35:41 +0200

    Minor change.

--- oommfc/hamiltonian/hamiltonian.py
+++ oommfc/hamiltonian/hamiltonian.py
@@ -4,7 +4,8 @@ import micromagneticmodel as mm
 class Hamiltonian(mm.Hamiltonian):
     @property
     def _script(self):
-        mif = ""
+        mif = ''
         for term in self.terms:
             mif += term._script
+
         return mif
--- oommfc/system.py
+++ oommfc/system.py
@@ -2,25 +2,12 @@ import micromagneticmodel as mm
 
 
 class System(mm.System):
-    """Micromagnetic system oject.
-
-    Parameters
-    ----------
-    name : str
-
-    Examples
-    --------
-    Creating a simple system object.
-
-    >>> import oommfc as oc
-    >>> system = oc.System(name="my_system")
-
-    """
     @property
     def _script(self):
         mif = self.m.mesh._script
         mif += self.hamiltonian._script
+
         return mif
 
     def total_energy(self):
-        return self.dt.tail(1)["E"][0]
+        return self.dt.tail(1)['E'][0]
