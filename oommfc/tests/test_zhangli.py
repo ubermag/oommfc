1f444983502b11a90d8e06b82b93a3813ab9a901
Marijan Beg <m.beg@soton.ac.uk>
2020-03-05 16:52:01 +0000

    ZhangLi.

--- oommfc/tests/test_zhangli.py
+++ oommfc/tests/test_zhangli.py
@@ -1,23 +1,24 @@
 import os
 import shutil
-import random
 import numpy as np
 import oommfc as oc
 import discretisedfield as df
+import micromagneticmodel as mm
 
 
 class TestZhangLi:
     def setup(self):
-        self.p1 = (-5e-9, -5e-9, -3e-9)
-        self.p2 = (5e-9, 5e-9, 3e-9)
+        p1 = (-5e-9, -5e-9, -3e-9)
+        p2 = (5e-9, 5e-9, 3e-9)
+        self.region = df.Region(p1=p1, p2=p2)
         self.n = (10, 10, 10)
-        self.regions = {'r1': df.Region(p1=(-5e-9, -5e-9, -3e-9),
-                                        p2=(5e-9, 0, 3e-9)),
-                        'r2': df.Region(p1=(-5e-9, 0, -3e-9),
-                                        p2=(5e-9, 5e-9, 3e-9))}
+        self.subregions = {'r1': df.Region(p1=(-5e-9, -5e-9, -3e-9),
+                                           p2=(5e-9, 0, 3e-9)),
+                           'r2': df.Region(p1=(-5e-9, 0, -3e-9),
+                                           p2=(5e-9, 5e-9, 3e-9))}
 
     def test_scalar_scalar(self):
-        name = 'zl_scalar_scalar'
+        name = 'zhangli_scalar_scalar'
         if os.path.exists(name):
             shutil.rmtree(name)
 
@@ -26,24 +27,24 @@ class TestZhangLi:
         H = (0, 0, 1e5)
         Ms = 1e6
 
-        mesh = oc.Mesh(p1=self.p1, p2=self.p2, n=self.n)
+        mesh = df.Mesh(region=self.region, n=self.n)
 
-        system = oc.System(name=name)
-        system.hamiltonian = oc.Zeeman(H=H)
-        system.dynamics = oc.ZhangLi(u=u, beta=beta)
+        system = mm.System(name=name)
+        system.energy = mm.Zeeman(H=H)
+        system.dynamics = mm.ZhangLi(u=u, beta=beta)
         system.m = df.Field(mesh, dim=3, value=(0, 0.1, 1), norm=Ms)
 
         td = oc.TimeDriver()
         td.drive(system, t=0.2e-9, n=50)
 
         # u is zero, nothing should change.
-        value = system.m(mesh.random_point())
+        value = system.m(mesh.region.random_point())
         assert np.linalg.norm(np.cross(value, (0, 0.1*Ms, Ms))) < 1e-3
 
-        system.delete()
+        td.delete(system)
 
     def test_dict_scalar(self):
-        name = 'zl_dict_scalar'
+        name = 'zhangli_dict_scalar'
         if os.path.exists(name):
             shutil.rmtree(name)
 
@@ -52,12 +53,12 @@ class TestZhangLi:
         beta = 0.5
         Ms = 1e6
 
-        mesh = oc.Mesh(p1=self.p1, p2=self.p2, n=self.n,
-                       regions=self.regions)
+        mesh = df.Mesh(region=self.region, n=self.n,
+                       subregions=self.subregions)
 
-        system = oc.System(name=name)
-        system.hamiltonian = oc.Zeeman(H=H)
-        system.dynamics = oc.ZhangLi(u=u, beta=beta)
+        system = mm.System(name=name)
+        system.energy = mm.Zeeman(H=H)
+        system.dynamics = mm.ZhangLi(u=u, beta=beta)
         system.m = df.Field(mesh, dim=3, value=(0, 0.1, 1), norm=Ms)
 
         td = oc.TimeDriver()
@@ -71,14 +72,14 @@ class TestZhangLi:
         value = system.m((1e-9, 4e-9, 3e-9))
         assert np.linalg.norm(np.subtract(value, (0, 0, Ms))) > 1
 
-        system.delete()
+        td.delete(system)
 
     def test_field_scalar(self):
-        name = 'zl_field_scalar'
+        name = 'zhangli_field_scalar'
         if os.path.exists(name):
             shutil.rmtree(name)
 
-        mesh = oc.Mesh(p1=self.p1, p2=self.p2, n=self.n)
+        mesh = df.Mesh(region=self.region, n=self.n)
 
         def u_fun(pos):
             x, y, z = pos
@@ -92,9 +93,9 @@ class TestZhangLi:
         beta = 0.5
         Ms = 1e6
 
-        system = oc.System(name=name)
-        system.hamiltonian = oc.Zeeman(H=H)
-        system.dynamics = oc.ZhangLi(u=u, beta=beta)
+        system = mm.System(name=name)
+        system.energy = mm.Zeeman(H=H)
+        system.dynamics = mm.ZhangLi(u=u, beta=beta)
         system.m = df.Field(mesh, dim=3, value=(0, 0.1, 1), norm=Ms)
 
         td = oc.TimeDriver()
@@ -108,4 +109,4 @@ class TestZhangLi:
         value = system.m((1e-9, 4e-9, 3e-9))
         assert np.linalg.norm(np.subtract(value, (0, 0, Ms))) > 1
 
-        system.delete()
+        td.delete(system)
