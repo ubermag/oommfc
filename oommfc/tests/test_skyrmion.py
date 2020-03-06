8637f664c66ba7b4a1327cdadd6e41d73881ba49
Marijan Beg <m.beg@soton.ac.uk>
2020-03-06 10:45:21 +0000

    Tests.

--- oommfc/tests/test_skyrmion.py
+++ oommfc/tests/test_skyrmion.py
@@ -3,6 +3,7 @@ import shutil
 import pytest
 import oommfc as oc
 import discretisedfield as df
+import micromagneticmodel as mm
 
 
 def test_skyrmion():
@@ -19,16 +20,16 @@ def test_skyrmion():
     u = (0, 0, 1)
     H = (0, 0, 2e5)
 
-    mesh = oc.Mesh(p1=(-50e-9, -50e-9, 0),
-                   p2=(50e-9, 50e-9, 10e-9),
-                   cell=(5e-9, 5e-9, 5e-9))
+    p1 = (-50e-9, -50e-9, 0)
+    p2 = (50e-9, 50e-9, 10e-9)
+    cell = (5e-9, 5e-9, 5e-9)
+    region = df.Region(p1=p1, p2=p2)
+    mesh = df.Mesh(p1=p1, p2=p2, cell=cell)
 
-    system = oc.System(name=name)
-    system.hamiltonian = oc.Exchange(A=A) + \
-        oc.DMI(D=D, crystalclass='Cnv') + \
-        oc.UniaxialAnisotropy(K1=K, u=u) + \
-        oc.Demag() + \
-        oc.Zeeman(H=H)
+    system = mm.System(name=name)
+    system.energy = (mm.Exchange(A=A) + mm.DMI(D=D, crystalclass='Cnv') +
+                     mm.UniaxialAnisotropy(K=K, u=u) + mm.Demag() +
+                     mm.Zeeman(H=H))
 
     def Ms_fun(pos):
         x, y, z = pos
@@ -44,7 +45,7 @@ def test_skyrmion():
         else:
             return (0, 0.1, 1)
 
-    system.m = df.Field(mesh, value=m_init, norm=Ms_fun)
+    system.m = df.Field(mesh, dim=3, value=m_init, norm=Ms_fun)
 
     md = oc.MinDriver()
     md.drive(system)
@@ -58,4 +59,4 @@ def test_skyrmion():
     value = system.m((50e-9, 0, 0))
     assert value[2]/Ms > 0.5
 
-    system.delete()
+    md.delete(system)
