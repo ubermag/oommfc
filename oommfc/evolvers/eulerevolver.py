7c392cc0f05c573c10528ff69180e676dddd9ac3
Marijan Beg <m.beg@soton.ac.uk>
2020-03-06 11:01:35 +0000

    Tests.

--- oommfc/evolvers/eulerevolver.py
+++ oommfc/evolvers/eulerevolver.py
@@ -27,25 +27,25 @@ class EulerEvolver(mm.Evolver):
     AttributeError: ...
 
     """
-    _allowed_kwargs = ['alpha',
-                       'gamma_LL',
-                       'gamma_G',
-                       'do_precess',
-                       'min_timestep',
-                       'max_timestep',
-                       'start_dm',
-                       'error_rate',
-                       'absolute_step_error',
-                       'relative_step_error',
-                       'step_headroom']
+    _allowed_attributes = ['alpha',
+                           'gamma_LL',
+                           'gamma_G',
+                           'do_precess',
+                           'min_timestep',
+                           'max_timestep',
+                           'start_dm',
+                           'error_rate',
+                           'absolute_step_error',
+                           'relative_step_error',
+                           'step_headroom']
 
     @property
     def _script(self):
         mif = '# EulerEvolver\n'
         mif += 'Specify Oxs_EulerEvolve:evolver {\n'
-        for kwarg in self._allowed_kwargs:
-            if hasattr(self, kwarg):
-                mif += f'  {kwarg} {getattr(self, kwarg)}\n'
+        for attr in self._allowed_attributes:
+            if hasattr(self, attr):
+                mif += f'  {attr} {getattr(self, attr)}\n'
         mif += '}\n\n'
 
         return mif
