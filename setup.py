b0beff419074e4da1d0463a8c3d63ea48969842e
Marijan Beg <m.beg@soton.ac.uk>
2020-07-10 11:05:44 +0100

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name='oommfc',
-    version='0.8.23',
+    version='0.8.24',
     description=('OOMMF calculator.'),
     long_description=long_description,
     long_description_content_type='text/markdown',
