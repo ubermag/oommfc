f504d19a6e12f38405a74e87759a146ef91acac9
marijanbeg <m.beg@soton.ac.uk>
2021-01-03 19:39:37 +0000

    Update version.

--- oommfc/__init__.py
+++ oommfc/__init__.py
@@ -10,7 +10,6 @@ from .evolvers import CGEvolver, EulerEvolver, RungeKuttaEvolver, \
     SpinTEvolver, SpinXferEvolver
 
 __version__ = pkg_resources.get_distribution(__name__).version
-__dependencies__ = pkg_resources.require(__name__)
 
 
 def test():
