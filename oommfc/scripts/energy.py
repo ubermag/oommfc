686f52af754c434dcbe1b380bc15bee34aa51b08
Martin Lang <m.lang@soton.ac.uk>
2021-05-26 09:31:08 +0200

    Rename variable.

--- oommfc/scripts/energy.py
+++ oommfc/scripts/energy.py
@@ -176,24 +176,24 @@ def demag_script(term, system):
 def dmi_script(term, system):
     if term.crystalclass in ['T', 'O']:
         oxs = 'Oxs_DMI_T'
-    elif (cls := term.crystalclass) in ['D2d_x', 'D2d_y', 'D2d_z', 'D2d']:
-        if term.crystalclass == 'D2d':
+    elif (tcc := term.crystalclass) in ['D2d_x', 'D2d_y', 'D2d_z', 'D2d']:
+        if tcc == 'D2d':
             warnings.warn('Use of `D2d` is deprecated; use `D2d_z` instead.',
                           FutureWarning)
-            cls = 'D2d_z'
-        oxs = f'Oxs_DMI_{cls}'
-    elif (cls := term.crystalclass) in ['Cnv_x', 'Cnv_y', 'Cnv_z', 'Cnv']:
+            tcc = 'D2d_z'
+        oxs = f'Oxs_DMI_{tcc}'
+    elif (tcc := term.crystalclass) in ['Cnv_x', 'Cnv_y', 'Cnv_z', 'Cnv']:
         if sys.platform == 'win32' and system.m.mesh.bc == '':
-            if cls in ['Cnv_x', 'Cnv_y']:
-                msg = f'Crystalclass {cls} is not supported on Windows.'
+            if tcc in ['Cnv_x', 'Cnv_y']:
+                msg = f'Crystalclass {tcc} is not supported on Windows.'
                 raise RuntimeError(msg)
             oxs = 'Oxs_DMExchange6Ngbr'
         else:
-            if term.crystalclass == 'Cnv':
+            if tcc == 'Cnv':
                 msg = 'Use of `Cnv` is deprecated; use `Cnv_z` instead.'
                 warnings.warn(msg, FutureWarning)
-                cls = 'Cnv_z'
-            oxs = f'Oxs_DMI_{cls}'
+                tcc = 'Cnv_z'
+            oxs = f'Oxs_DMI_{tcc}'
 
     mif = f'# DMI of crystallographic class {term.crystalclass}\n'
     mif += f'Specify {oxs}:{term.name} {{\n'
