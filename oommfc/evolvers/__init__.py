9c582d2af3061cd9a035d02f8cc49fe2e4233ec6
Martin Lang <m.lang@soton.ac.uk>
2021-06-25 10:01:31 +0200

    New evolvers Xf_ThermHeunEvolver and Xf_ThermSpinXferEvolver.

--- oommfc/evolvers/__init__.py
+++ oommfc/evolvers/__init__.py
@@ -4,3 +4,5 @@ from .rungekuttaevolver import RungeKuttaEvolver
 from .spintevolver import SpinTEvolver
 from .spinxferevolver import SpinXferEvolver
 from .uhh_thetaevolver import UHH_ThetaEvolver
+from .xf_thermheunevolver import Xf_ThermHeunEvolver
+from .xf_thermspinxferevolver import Xf_ThermSpinXferEvolvor
