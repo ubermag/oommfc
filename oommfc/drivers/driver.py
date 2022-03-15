0ad109f1402dae67d603c738be15b20e5c9a738e
Martin Lang <m.lang@soton.ac.uk>
2022-03-15 17:17:38 +0100

    Add explanatory comment

--- oommfc/drivers/driver.py
+++ oommfc/drivers/driver.py
@@ -229,6 +229,7 @@ class Driver(mm.Driver):
                 # test_sample-Oxs_TimeDriver-Magnetization-01-0000008.omf
                 omffiles = glob.iglob(f'{system.name}*.omf')
                 lastomffile = sorted(omffiles)[-1]
+                # pass Field.array instead of Field for better performance
                 system.m.value = df.Field.fromfile(lastomffile).array
 
                 # Update system's datatable.
