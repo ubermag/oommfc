acac8b79c7fee1d7681bd664063985d91a968696
Martin Lang <m.lang@soton.ac.uk>
2022-06-24 15:22:06 +0200

    Update repository metadata

--- tasks.py
+++ tasks.py
@@ -2,7 +2,6 @@
 import os
 import shutil
 
-import iniconfig
 import pytest
 import tomli
 from invoke import Collection, Exit, task
@@ -26,23 +25,24 @@ def unittest(c):
 @task
 def coverage(c):
     """Run unittests with coverage."""
-    result = pytest.main(["-v", "--cov", "oommfc", "--cov-report", "xml"])
+    result = pytest.main(["-v", "--cov", "oommfc",
+                          "--cov-report", "xml"])
     raise Exit(code=result)
 
 
 @task
 def docs(c):
     """Run doctests."""
-    result = pytest.main(
-        ["-v", "--doctest-modules", "--ignore", "oommfc/tests", "oommfc"]
-    )
+    result = pytest.main(["-v", "--doctest-modules", "--ignore",
+                          "oommfc/tests", "oommfc"])
     raise Exit(code=result)
 
 
 @task
 def ipynb(c):
     """Test notebooks."""
-    result = pytest.main(["-v", "--nbval", "--sanitize-with", "nbval.cfg", "docs"])
+    result = pytest.main(["-v", "--nbval", "--sanitize-with", "nbval.cfg",
+                          "docs"])
     raise Exit(code=result)
 
 
@@ -106,13 +106,8 @@ def release(c):
         if e.code != pytest.ExitCode.OK:
             raise e
 
-    version = iniconfig.IniConfig("setup.cfg").get("metadata", "version")
-    # sanity checks while we have two places containing the version.
     with open("pyproject.toml", "rb") as f:
-        toml_version = tomli.load(f)["project"]["version"]
-    assert (
-        toml_version == version
-    ), "Different versions in pyproject.toml and setup.cfg. Aborting."
+        version = tomli.load(f)["project"]["version"]
 
     c.run(f"git tag {version}")  # fails if the tag exists
     c.run("git tag -f latest")  # `latest` tag for binder
